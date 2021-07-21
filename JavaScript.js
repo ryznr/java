ea5c6eb1d4ad58985caf6057a210b82e62dbff17
ryznr <87032073+ryznr@users.noreply.github.com>
2021-07-21 19:44:09 +0700

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -4,11 +4,12 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Time right now is: </title>
-<script>
-var time = new Date();
-console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
-</script>
 </head>
 <body>
+<script>
+let d = new Date();
+document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
+"</h1>"
+</script>
 </body>
 </html>
